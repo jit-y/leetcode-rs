e6d7f80312a5c81e2418d890aaccfc4420f10b0e
YUJI takahashi <jit.y.31@gmail.com>
2020-02-25 23:18:54 +0900

    pascals triangle ii

---
+++ examples/pascals_triangle_ii.rs
@@ -0,0 +1,38 @@
+struct Solution {}
+impl Solution {
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        let mut result = Vec::new();
+
+        if row_index < 0 {
+            return result;
+        }
+
+        result.push(1);
+        if row_index == 0 {
+            return result;
+        }
+
+        result.push(1);
+        if row_index == 1 {
+            return result;
+        }
+
+        for _ in 2..=row_index {
+            let mut current = vec![1, 1];
+            for j in 0..(result.len() - 1) {
+                current.insert(1, result[j] + result[j + 1]);
+            }
+
+            result = current.clone();
+        }
+
+        result
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::get_row(2), vec![1, 2, 1]);
+    assert_eq!(Solution::get_row(3), vec![1, 3, 3, 1]);
+    assert_eq!(Solution::get_row(4), vec![1, 4, 6, 4, 1]);
+    assert_eq!(Solution::get_row(5), vec![1, 5, 10, 10, 5, 1]);
+}
