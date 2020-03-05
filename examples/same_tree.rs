4068f4e045ae6a5729f40f3d580772084781b834
YUJI takahashi <jit.y.31@gmail.com>
2020-03-05 23:32:32 +0900

    same tree

---
+++ examples/same_tree.rs
@@ -0,0 +1,141 @@
+// https://leetcode.com/problems/same-tree/
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+struct Solution {}
+impl Solution {
+    pub fn is_same_tree(
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> bool {
+        if p.is_some() && q.is_none() {
+            return false;
+        }
+
+        if p.is_none() && q.is_some() {
+            return false;
+        }
+
+        if let Some(p_ref) = p {
+            if let Some(q_ref) = q {
+                if p_ref != q_ref {
+                    return false;
+                }
+
+                let p_borrow = p_ref.borrow();
+                let q_borrow = p_ref.borrow();
+
+                Self::is_same_tree(p_borrow.left.clone(), q_borrow.left.clone());
+                Self::is_same_tree(p_borrow.right.clone(), q_borrow.right.clone());
+            }
+        }
+
+        true
+    }
+}
+
+fn main() {
+    assert_eq!(
+        Solution::is_same_tree(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+            }))),
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+            }))),
+        ),
+        true
+    );
+    assert_eq!(
+        Solution::is_same_tree(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+                right: None,
+            }))),
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+            }))),
+        ),
+        false
+    );
+    assert_eq!(
+        Solution::is_same_tree(
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 1,
+                    left: None,
+                    right: None
+                }))),
+            }))),
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 1,
+                    left: None,
+                    right: None
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: None
+                }))),
+            }))),
+        ),
+        false
+    );
+
+    assert_eq!(Solution::is_same_tree(None, None), true);
+}
