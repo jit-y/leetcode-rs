058ad7b47ac1fdf20d040d76e7832245869b380a
YUJI takahashi <jit.y.31@gmail.com>
2020-03-07 23:48:34 +0900

    palindrome linked list

--- examples/palindrome_linked_list.rs
+++ examples/palindrome_linked_list.rs
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/palindrome-linked-list/
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -28,6 +29,46 @@ impl Solution {
 
         x == y
     }
+
+    pub fn is_palindrome_2(head: Option<Box<ListNode>>) -> bool {
+        let mut node = &head;
+        let mut reverse = None;
+
+        while let Some(v) = &node {
+            let mut n = ListNode::new(v.val);
+            n.next = reverse;
+
+            reverse = Some(Box::new(n));
+            node = &v.next;
+        }
+
+        head == reverse
+    }
+
+    pub fn is_palindrome_3(head: Option<Box<ListNode>>) -> bool {
+        if head.is_none() {
+            return true;
+        }
+        let mut node = head;
+        let mut reverse = None;
+
+        while let Some(v) = node.as_mut() {
+            if reverse == v.next {
+                return true;
+            }
+
+            let n = v.next.take();
+            v.next = reverse;
+            reverse = node;
+            node = n;
+
+            if reverse == node {
+                return true;
+            }
+        }
+
+        false
+    }
 }
 
 fn main() {
@@ -53,6 +94,36 @@ fn main() {
     );
     assert_eq!(
         Solution::is_palindrome(Some(Box::new(ListNode {
+            val: -129,
+            next: Some(Box::new(ListNode {
+                val: -129,
+                next: None
+            }))
+        }))),
+        true
+    );
+    assert_eq!(
+        Solution::is_palindrome_2(Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode { val: 2, next: None }))
+        }))),
+        false
+    );
+    assert_eq!(
+        Solution::is_palindrome_2(Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 2,
+                    next: Some(Box::new(ListNode { val: 1, next: None }))
+                }))
+            }))
+        }))),
+        true
+    );
+    assert_eq!(
+        Solution::is_palindrome_2(Some(Box::new(ListNode {
             val: -129,
             next: Some(Box::new(ListNode {
                 val: -129,
